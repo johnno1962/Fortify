57806701469b5d6f1375ea0afb22cd924da881e3
John Holdsworth <github@johnholdsworth.com>
2020-10-28 15:58:28 +0100

    Updated for Swift 5

--- Package.swift
+++ Package.swift
@@ -1,7 +1,25 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+import Foundation
+
+let name = URL(fileURLWithPath: #file)
+    .deletingLastPathComponent().lastPathComponent
 
 let package = Package(
-    name: "Fortify",
+    name: name,
+    products: [
+        .library(
+            name: name,
+            targets: [name]),
+    ],
     dependencies: [
+    ],
+    targets: [
+        .target(
+            name: name,
+            dependencies: [],
+            path: "Sources/")
     ]
 )
---
+++ fortifyapp/main.swift
@@ -0,0 +1,88 @@
+//
+//  main.swift
+//  fortifyapp
+//
+//  Created by John Holdsworth on 26/04/2018.
+//
+
+import Foundation
+import Fortify
+
+do {
+    try Fortify.exec {
+        var a: String!
+        a = a!
+    }
+}
+catch {
+    NSLog("Unwrap error: \(error)")
+}
+
+print("Hello, World!")
+
+func f( g: () -> Void ) {
+    g()
+}
+
+func g( h: () -> Void ) {
+    f( g: h )
+}
+
+do {
+    try Fortify.exec {
+        g( h: {
+            class B{}
+            class C{
+                func x(){}
+            }
+            var b: Any = B()
+            var d = b as! C
+            d.x()
+        })
+    }
+}
+catch {
+    print("\(error)")
+}
+
+do {
+    try Fortify.exec {
+        var a: String?
+        NSLog(a!)
+    }
+}
+catch {
+    print("\(error)")
+}
+
+do {
+    try Fortify.exec {
+        var a: String?
+        NSLog(a!)
+    }
+}
+catch {
+    print("\(error)")
+}
+
+do {
+    try Fortify.exec {
+        class B{}
+        class C{}
+        var d = B() as! C
+    }
+}
+catch {
+    print("\(error)")
+}
+
+do {
+    try Fortify.exec {
+        class B{}
+        class C{}
+        var d = B() as! C
+    }
+}
+catch {
+    print("\(error)")
+}
